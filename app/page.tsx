7eb1e29e9273714e5bc4183d46bcf26e7c767174
Hiro527 <hiro527.dev@gmail.com>
2025-04-21 11:45:20 +0900

    Add: sample app

---
+++ app/page.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import { Box, TextField, Typography } from '@mui/material';
+import { useState } from 'react';
+
+export default function Home() {
+    const [name, setName] = useState('');
+    return (
+        <Box
+            sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                width: '100vw',
+                height: '100svh',
+                backgroundColor: '#f0f0f0',
+            }}
+        >
+            <Typography
+                variant="h5"
+                sx={{
+                    fontWeight: 'bold',
+                }}
+            >
+                {name || '名無し'}さん、こんにちは！
+            </Typography>
+            <TextField
+                sx={{
+                    width: '300px',
+                    mt: 2,
+                }}
+                label="名前"
+                placeholder="あなたの名前は？"
+                onChange={(e) => setName(e.target.value)}
+            ></TextField>
+        </Box>
+    );
+}
---
+++ theme/theme.ts
@@ -0,0 +1,12 @@
+'use client';
+import { createTheme } from '@mui/material';
+
+const theme = createTheme({
+    typography: {
+        allVariants: {
+            color: '#222222',
+        },
+    },
+});
+
+export default theme;
